d481185ecfe8aae69dabc3a91a0ddb3f36c92001
unbreakablesoul0 <mamidisettimanojakshaya@gmail.com>
2026-01-30 08:10:44 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/Lab1.FEWD/',
+  base: '/Lab1.FEWD/',
   plugins: [react()],
 })
